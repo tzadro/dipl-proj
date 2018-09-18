2920fee013106118033cfdfa41446a4ffffed31f
tzadro <tzadro94@gmail.com>
2018-09-18 20:16:05 +0200

    Slight refactor

--- NEAT.py
+++ NEAT.py
@@ -1,4 +1,5 @@
-# todo: add interspecies mating probability; remove species after their best fitness doesn't improve for some number of turns
+# todo: add interspecies mating probability;
+# todo: remove species after their best fitness doesn't improve for some number of turns
 
 from Config import config
 from Population import Population
@@ -7,9 +8,12 @@ import gym
 env = gym.make('CartPole-v0')
 
 population = Population()
+
 for i in range(config.num_iter):
-	print('Generation: {:d}, best fitness: {:.2f}'.format(i, population.evaluate_fitness(env)))
+	best_fitness = population.evaluate_fitness(env)
 	population.speciate()
 	population.breed_new_generation()
+
+	print('Generation: {:d}, best fitness: {:.2f}'.format(i, best_fitness))
 
 env.render(close=True)
