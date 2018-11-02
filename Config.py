172ecda4bcca32d8eff7171345f330639b3dd0d4
tzadro <tzadro94@gmail.com>
2018-11-02 16:10:56 +0100

    Reorder hyperparameters; Refactor crossover skip

--- Config.py
+++ Config.py
@@ -1,37 +1,38 @@
 class Config:
 	def __init__(self):
-		self.connection_mutation_probability = 0.5
-		self.perturbation_probability = 0.9
-		self.new_node_probability = 0.003
-		self.new_connection_probability = 0.005
-		self.step = 0.25
+		self.pop_size = 200
 		self.c1 = 1.0
 		self.c2 = 1.0
 		self.c3 = 0.4
-		self.normalize = True
 		self.compatibility_threshold = 2.0
-		self.crossover_probability = 0.75
+		self.max_num_generations_before_improvement = 30
+		self.min_num_individuals_for_elitism = 5
+		self.connection_mutation_probability = 0.5
+		self.perturbation_probability = 0.9
 		self.stay_disabled_probability = 0.75
-		self.pop_size = 200
+		self.skip_crossover_probability = 0.25
+		self.new_node_probability = 0.003
+		self.new_connection_probability = 0.005
+		self.sigmoid_coef = 1.0
+
 		self.num_iter = 501
+		self.step = 0.25
 		self.survival_threshold = 0.9
 		self.new_mu = 0.0
 		self.new_sigma = 1.0
 		self.step_mu = 0.0
 		self.step_sigma = 0.75
-		self.min_num_individuals_for_elitism = 5
-		self.max_num_generations_before_improvement = 30
 		self.num_evaluation_runs = 1
-		self.sigmoid_coef = 1.0
+		self.normalize = True
 
 		self.num_starting_hidden_nodes = 8
 		self.fixed_topology = True
 
-		self.visualize_best_networks = True
-		self.visualize_every = 100
-		self.verbose = True
 		self.network_canvas_height = 100
 		self.network_canvas_width = 100
+		self.visualize_every = 100
+		self.visualize_best_networks = True
+		self.verbose = True
 
 		self.num_starting_nodes = None
 		self.next_node_key = None
--- Species.py
+++ Species.py
@@ -37,10 +37,10 @@ class Species:
 		self.individuals.sort(key=key)
 
 	def breed_child(self, generation_new_nodes, generation_new_connections):
-		if random.random() < config.crossover_probability and len(self.individuals) > 1:
-			child = crossover(self.select(2))
-		else:
+		if len(self.individuals) == 1 or random.random() < config.skip_crossover_probability:
 			child = copy.deepcopy(self.select())
+		else:
+			child = crossover(self.select(2))
 
 		child.mutate(generation_new_nodes, generation_new_connections)
 		return child
